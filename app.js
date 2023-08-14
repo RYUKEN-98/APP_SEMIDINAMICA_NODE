d7e29aa4183f52ace4ccfd595ccd6715a2294e24
DIEGO MAZO <damazo1981@gmail.com>
2023-08-14 18:01:37 -0500

    2023/08/14

--- app.js
+++ app.js
@@ -11,7 +11,29 @@ const port = 9696;
 // Mostrar contenido de la carpeta public 
 app.use(express.static('public'));
 
-app.get('/right-sidebar', (req, res) => {
+app.get('/left-sidebar', (req, res) => {
     //Utilizar el res, para responder a la peticion del usuario
     res.sendfile(__dirname + '/public/left-sidebar.html')
+});
+
+app.get('/right-sidebar', (req, res) => {
+    //Utilizar el res, para responder a la peticion del usuario
+    res.sendfile(__dirname + '/public/right-sidebar.html')
+});
+
+app.get('/no-sidebar', (req, res) => {
+    //Utilizar el res, para responder a la peticion del usuario
+    res.sendfile(__dirname + '/public/no-sidebar.html')
+});
+
+app.get('*', (req, res) => {
+    //El comodin o *, es para validar la busqueda de una pagina
+    //Utilizar el res, para responder a la peticion del usuario
+    res.sendfile(__dirname + '/public/404.html')
+});
+
+
+//* se necesita activar el puerto pára el WebServer
+app.listen(port,()=> {
+    console.log('Webserver activo en el puerto ${port}?')
 })
