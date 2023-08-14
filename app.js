650cf3b9ac385f1fef7e442a7ba5c3d411045a83
DIEGO MAZO <damazo1981@gmail.com>
2023-08-14 15:13:22 -0500

    2023

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 
 
-console.log('Hola Nodejs');
-console.log('Hola Diego');
-console.log('Hola Nodejs');
-console.log('Hola Nodejs');
-console.log('Hola Nodejs');
-console.log('Hola Nodejs');
+/*Importar paquetes como el express
+Para crear Webserver en un entorno Local 
+*/
 
+const express = require('express');
+const app = express();
+const port = 9696;
 
+// Mostrar contenido de la carpeta public 
+app.use(express.static('public'));
+
+app.get('/right-sidebar', (req, res) => {
+    //Utilizar el res, para responder a la peticion del usuario
+    res.sendfile(__dirname + '/public/left-sidebar.html')
+})
